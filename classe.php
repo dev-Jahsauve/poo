735e71e7fcff0faeddc39b7430d47b16d5a3f9bd
dev-Jahsauve <jahsauveyimele@gmail.com>
2025-08-09 02:18:10 +0100

    mon premier push

---
+++ classe.php
@@ -0,0 +1,16 @@
+<?php
+
+class CompteBancaire{
+    private $solde=0;
+    public function depot($montant){
+        if($montant>0){
+            $this->solde+=$montant;
+        }
+    }
+    public function getsolde(){
+        return $this->solde;
+    }
+}
+$compte= new CompteBancaire();
+$compte->depot(500);
+echo $compte->getsolde()."FCFA";
